7c7b8ecfc0456f419830bec5878759a0226c9382
Mikhail Sprygin <wave103x@gmail.com>
2022-10-21 18:18:33 +0300

    feat: add full responsive

--- smoke-tunnel/src/index.js
+++ smoke-tunnel/src/index.js
@@ -1,1 +1,13 @@
 import './scss/main.scss';
+
+const form = document.querySelector('.form-consult');
+const btn = document.querySelector('.form-button');
+
+btn.addEventListener('click', (e) => {
+    if (form.firstElementChild.value && btn.previousElementSibling.value) {
+    alert(`Name: ${form.firstElementChild.value}, \nPhone: ${btn.previousElementSibling.value}`);
+    } else {
+        alert('Fill gaps')
+    }
+    e.preventDefault();
+})
