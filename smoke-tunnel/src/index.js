f4e6098692fc9a853cf5783e8547b8572f1564c9
Mikhail Sprygin <wave103x@gmail.com>
2022-10-24 11:55:21 +0300

    feat: padding fixes, header intercativity

--- smoke-tunnel/src/index.js
+++ smoke-tunnel/src/index.js
@@ -3,6 +3,7 @@ import './scss/main.scss';
 const form = document.querySelector('.form-consult');
 const btn = document.querySelector('.form-button');
 
+//simple form interactive
 btn.addEventListener('click', (e) => {
     if (form.firstElementChild.value && btn.previousElementSibling.value) {
     alert(`Name: ${form.firstElementChild.value}, \nPhone: ${btn.previousElementSibling.value}`);
@@ -11,3 +12,17 @@ btn.addEventListener('click', (e) => {
     }
     e.preventDefault();
 })
+
+//onclick event on header rounded buttons
+Array.from(document.querySelectorAll('.circle-btn')).forEach(e => e.addEventListener('click', () => e.classList.toggle('circle-btn_active')))
+
+//change content according to layout
+const sendBtn = document.querySelector('.form-button');
+const formHeading = document.querySelector('.h3_consult');
+if (window.innerWidth < 769) {
+    sendBtn.textContent = 'о    ставить заявку';
+    formHeading.textContent = 'Необходима консультация';
+} else {
+    sendBtn.textContent = 'отправить';
+    formHeading.textContent = 'Необходима консультация?';
+}
